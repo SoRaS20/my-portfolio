cbc72e22a1949d68ee5a732ae6cd643fe0da36b3
SoRaS20 <sohanurrahman621@gmail.com>
2026-02-02 17:18:46 +0600

    fix: Azure Static Web Apps CI/CD - configure static export

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemeProviderProps } from 'next-themes'
+
+type ThemeProviderProps = React.PropsWithChildren<NextThemeProviderProps>
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
